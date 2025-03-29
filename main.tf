69dcedf60d3b5df3cdd5513dcf6129a5e663b3b4
martial228 <martialmiza@gmail.com>
2025-03-29 12:29:47 -0600

    message

---
+++ main.tf
@@ -0,0 +1,10 @@
+resource "aws_lightsail_instance" "server1" {
+  name              = "dev-server"
+  blueprint_id      = "ubuntu_18_04"
+  bundle_id         = "medium_1_0"
+  availability_zone = "us-east-1a"
+  user_data         = <<-EOF
+              #!/bin/bash
+              echo "Hello, World!" > /home/ubuntu/hello-world.txt
+              EOF
+}
